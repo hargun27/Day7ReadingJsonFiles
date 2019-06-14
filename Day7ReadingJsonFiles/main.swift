91f4359dc90500ace762817d867bb4d526dddba9
hargunmarya <hargunmarya2726@gmail.com>
2019-06-14 18:17:38 -0400

    Initial Commit

---
+++ Day7ReadingJsonFiles/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day7ReadingJsonFiles
+//
+//  Created by Hargun Marya on 2019-06-14.
+//  Copyright © 2019 lambton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
